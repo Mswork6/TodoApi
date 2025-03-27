7d9df425d3b0bf18b91dd9e4ac533c90d17b5a3e
Mswork6 <denis.ch74@mail.ru>
2025-03-28 00:40:09 +0500

    fix: updated versions for .net 8.0

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0aa710f3debeeba71e2306a25c9cb59f833e83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d779b1dc86fc3378fae0db7a7a42d5908333f8ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
